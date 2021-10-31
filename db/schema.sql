4803f4950bf42999b26b960f4c567853a778f505
God of War <jamel.eggleston@gmail.com>
2021-10-31 13:59:12 -0400

    added department table

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,8 @@ DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 USE company_db;
 
+-- Department Table
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
