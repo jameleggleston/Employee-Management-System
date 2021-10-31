087945766f91dff7d84355537a889d71db794a84
God of War <jamel.eggleston@gmail.com>
2021-10-31 15:03:23 -0400

    created connection in index.js file

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// start here
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+
+const db_connection = mysql.createConnection(
+
+    {
+        host: "localhost",
+        port: "3306",
+        user: "root",
+        password: "Myworld12!",
+        database: "company_db",
+    },
+    console.log("Connected to company_db")
+);
+
+db_connection.connect(function (err) {
+    if (err) throw err;
+    start();
+})
