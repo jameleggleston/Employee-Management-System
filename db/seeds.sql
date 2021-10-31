94a13e3b66009f3b2dc726232bb9c6ea87598328
God of War <jamel.eggleston@gmail.com>
2021-10-31 14:30:55 -0400

    added role seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,15 @@ VALUES (1, "Board"),
        (3, "Engineering"),
        (4, "Finance"),
        (5, "Legal"); 
+
+-- Role seeds
+INSERT INTO role (department_id, title, salary)
+VALUES (1, "CEO", 15000000),
+       (2, "Sales Lead", 100000),
+       (2, "Salesperson", 80000),
+       (3, "Lead Engineer", 150000),
+       (3, "Software Engineer", 120000),
+       (4, "Accountant Manager", 160000),
+       (4, "Accountant", 125000),
+       (5, "Legal Team Lead", 250000),
+       (5, "Lawyer", 190000);
