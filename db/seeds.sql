4133f8bf11291d23debb25fc4ba5edae39b74140
God of War <jamel.eggleston@gmail.com>
2021-10-31 14:22:37 -0400

    added department seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,7 @@
--- start here
+-- Department seeds
+INSERT INTO department (id, name)
+VALUES (1, "Board"),
+       (2, "Sales"),
+       (3, "Engineering"),
+       (4, "Finance"),
+       (5, "Legal"); 
